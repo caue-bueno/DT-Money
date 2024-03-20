9822d5b03434d79a358814b9825cee25ba1a6e6c
caue-bueno <caue-bueno@live.com>
2024-03-20 20:11:34 -0300

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./styles/themes/default";
+import { GlobalStyles } from "./styles/global";
+import { Header } from "./components/Header";
+import { Transactions } from "./pages/Transactions";
+
+export function App() {
+
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyles />
+      <Transactions />
+    </ThemeProvider>
+    
+  )
+}
